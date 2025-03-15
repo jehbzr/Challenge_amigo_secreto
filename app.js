35ddec0aff0e527845250b26bc5502027b025dae
jehbzr <jessicabezerrra951@gmail.com>
2025-03-14 22:33:28 -0300

    criando um array

--- app.js
+++ app.js
@@ -1,20 +1,55 @@
-    let input = document.getElementById('amigo');
-    let lista = document.getElementById('listaAmigos');
-    let valor=input.value;
-    function adicionarAmigo(){
-  
+//criando um array para armazenar o nome dos amigos
+let amigos=[];
 
-        if(valor){
-            let amigo = document.createElement('ul');
-            amigo.textContent=valor;
-            lista.appendChild(amigo);
-            
-            input.value='';
-                    } else{
-                        alert ("Por favor, insira o nome de um amigo!");
-                    }
+// função para adicionar o nome dos amigos
+function adicionarAmigo(){
+    let incluiAmigo = document.getElementById('amigo');
+    let nomeDoAmigo= incluiAmigo.value.trim();
+     
+
+    // validar se um campo está vazio
+    if(nomeDoAmigo===""){
+        alert('Insira um nome,porfavor!');
+    }
+    if(amigos.includes(nomeDoAmigo)){
+
+        alert('Esse nome já está na lista!');
+        return;
+    }
+    amigos.push(nomeDoAmigo);
+    incluiAmigo.value="";
+
+    atualizaLista();
+
+}
+    
+
+// função para atualizar lista
+    function atualizaLista(){
+        let listaAmigos= document.getElementById('listaAmigos');
+        listaAmigos.innerHTML="";
+        let i=0; 
+
+            while(i < amigos.length){
+                let nome =document.createElement('li');
+                nome.textContent = amigos[i];
+                listaAmigos.appendChild(nome);
+                i++
+        }
 
     }
- function sortearAmigo(){
-        input.push(listaAmigos);
- }
+
+//função que sorteia o nome de um amigo
+
+    function sortearAmigo(){
+        if(amigos.length === 0){
+
+            alert('Não tem nomes a sortear,insira um nome.');
+        }
+       let indiceAleatorio = Math.floor(Math.random() * amigos.length);
+       let amigoSorteado = amigos[indiceAleatorio];
+
+       let resultado = document.getElementById('resultado');
+       resultado.innerHTML =`O amigo sorteado é: <strong>${amigoSorteado}</strong>`;
+
+    }
